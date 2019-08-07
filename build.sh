3f5986c2b619d7ef7e2f6324a3877245f71eb98f
Your Name <you@example.com>
2019-08-07 17:37:24 +0800

    modify  buildspec.yml build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
-set -ex
-#set registry
-#USERNAME= ECR 的位置
-USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
-# image name
-#IMAGE= Repo name
-IMAGE=project01-repo
-docker build -t $USERNAME/$IMAGE:latest .
-version=`cat VERSION`
-echo "version: $version"
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-docker push $USERNAME/$IMAGE:$version
-
+set -ex 
+#set registry 
+USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com 
+# image name 
+IMAGE=project01-repo 
+docker build -t $USERNAME/$IMAGE:latest . 
+version=`cat VERSION` 
+echo "version: $version" 
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version 
+docker push $USERNAME/$IMAGE:$version 
+ 
