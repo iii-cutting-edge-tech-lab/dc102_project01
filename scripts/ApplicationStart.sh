54c48a5f227b859a360ab7bbb395614081d80329
Your Name <you@example.com>
2019-08-09 11:27:14 +0800

    modify ApplicationStart.sh

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -p 80:5000 --name 'jupyter.vcloudlab.pro' -d 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/project01-repo:latest
+docker run -p 80:5000 --name 'jupyter.vcloudlab.pro' -d 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/project01-repo:latest start-notebook.sh --NotebookApp.token=''
+
